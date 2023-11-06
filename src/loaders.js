971f9360b9bdf44a119cb91587441b0493a04e19
Karla Lazarte <karlalazarte@Stephens-MacBook-Pro.local>
2023-11-06 18:49:21 -0500

    router.js file changed:

--- src/loaders.js
+++ src/loaders.js
@@ -1,16 +1,33 @@
 // YOUR DEPLOYED API BASE URL
+// const URL = 'https://rythmix-app.onrender.com'
+
+// //indexLoader => 
+// export const indexLoader = async () => {
+//     const response = await fetch(URL + "/playlists/")
+//     const playlists = await response.json()
+//     return playlists
+// }
+
+// //showLoader => 
+// export const showLoader = async ({params}) => {
+//     const response = await fetch(URL + `/playlists/${params.id}/`)
+//     const playlist = await response.json()
+//     return playlist
+// }
+
+// CLASS NOTES
 const URL = 'https://rythmix-app.onrender.com'
 
-//indexLoader => 
+//indexLoader => get all todos for index route
 export const indexLoader = async () => {
-    const response = await fetch(URL + "/playlists/")
-    const playlists = await response.json()
-    return playlists
-}
+  const response = await fetch(URL + "/todos/");
+  const todos = await response.json();
+  return todos;
+};
 
-//showLoader => 
-export const showLoader = async ({params}) => {
-    const response = await fetch(URL + `/playlists/${params.id}/`)
-    const playlist = await response.json()
-    return playlist
-}
+//showLoader => get a single todo for Show route
+export const showLoader = async ({ params }) => {
+  const response = await fetch(URL + `/todos/${params.id}/`);
+  const todo = await response.json();
+  return todo;
+};
